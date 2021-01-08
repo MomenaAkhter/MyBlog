82acd3b5295ad9d02d74481d6da624fb7e892209
Momena Akhter Shukhi <makhter.shukhi@gmail.com>
2021-01-08 12:04:46 +0600

    Add login and registration forms.

--- pages/login_register.php
+++ pages/login_register.php
@@ -1,1 +1,62 @@
-Login / Register
+<div class="login-register">
+    <div class="login">
+        <h1>Login</h1>
+
+        <form action="" method="post">
+            <div class="label">
+                Name:
+            </div>
+            <div>
+                <input type="text" name="name">
+            </div>
+
+            <div class="label">
+                Password:
+            </div>
+            <div>
+                <input type="text" name="password">
+            </div>
+
+            <div>
+                <input type="submit" value="Login">
+            </div>
+        </form>
+    </div>
+    <div class="register">
+        <h1>Register</h1>
+
+        <form action="" method="post">
+            <div class="label">
+                Name:
+            </div>
+            <div>
+                <input type="text" name="name">
+            </div>
+
+            <div class="label">
+                Password:
+            </div>
+            <div>
+                <input type="password" name="password">
+            </div>
+
+            <div class="label">
+                Password Confirmation:
+            </div>
+            <div>
+                <input type="password" name="password-confirmation">
+            </div>
+
+            <div class="label">
+                Email Address:
+            </div>
+            <div>
+                <input type="email" name="email-address">
+            </div>
+
+            <div>
+                <input type="submit" value="Register">
+            </div>
+        </form>
+    </div>
+</div>
