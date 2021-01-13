6d527cfe6efb749207dd4d080b524ed5b65c3869
Momena Akhter Shukhi <makhter.shukhi@gmail.com>
2021-01-13 21:03:42 +0600

    Add dashboard.

--- pages/admin_panel/index.php
+++ pages/admin_panel/index.php
@@ -1,1 +1,16 @@
 <h1>Admin Panel</h1>
+
+<div class="cards">
+    <div class="card">
+        <a href='articles.php'>Articles</a>
+    </div>
+    <div class="card">
+        <a href='menu_items.php'>Menu Items</a>
+    </div>
+    <div class="card">
+        <a href='users.php'>Users</a>
+    </div>
+    <div class="card">
+        <a href='comments.php'>Comments</a>
+    </div>
+</div>
--- pages/components/admin_panel/menu.php
+++ pages/components/admin_panel/menu.php
@@ -6,6 +6,7 @@ require_once __DIR__ . '/../../../models/User.php';
 <nav class="menu admin-panel-menu">
     <div class="left">
         <div class="title"><?php echo Config::get('site_name'); ?> Admin Panel</div>
+        <a href="index.php">Dashboard</a>
         <a href="articles.php">Articles</a>
         <a href="menu_items.php">Menu Items</a>
         <a href="users.php">Users</a>
