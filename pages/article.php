37e12cb14a06062f6366d774b6e0e47ad782bf35
Momena Akhter Shukhi <makhter.shukhi@gmail.com>
2021-01-14 17:56:42 +0600

    Display comment form.

--- pages/article.php
+++ pages/article.php
@@ -1,11 +1,21 @@
-<?php $article = $GLOBALS['article']; ?>
+<?php
+require_once __DIR__ . '/../helpers/datetime.php';
 
-<h1><?php echo $article['title']; ?></h1>
-<h4>Written by <i><?php echo $article['user_name']; ?></i></h4>
-<h4><?php echo $article['creation_timestamp']; ?></h4>
+$article = $GLOBALS['article'];
+$time_difference = diff($article['creation_timestamp']);
+?>
+
+<?php
+echo <<<EOT
+        <h1>{$article['title']}</h1>
+        <span>Written by <b>{$article['user_name']}</b> {$time_difference} ago</span>
+        <hr/>
+        EOT;
+?>
+
 
 <div>
-    <?php echo $article['body']; ?>
+    <?php echo nl2br($article['body']); ?>
 </div>
 
 <hr />
@@ -13,10 +23,28 @@
 <?php
 if ($article['comments_enabled'] == 1) {
     require_once __DIR__ . '/../models/Comment.php';
-    require_once __DIR__ . '/../helpers/datetime.php';
+    require_once __DIR__ . '/../models/User.php';
 
-    echo "<div class='comments'>";
     $comments = Comment::filter($article['id']);
+    $comments_count = count($comments);
+
+    echo <<<EOT
+        <h2>Comments ($comments_count)</h1>
+        EOT;
+
+    if ($user = User::isLoggedIn()) {
+        echo <<<EOT
+        <form action='#'>
+            <textarea placeholder='Type your message here'></textarea>
+            <input type='submit' value='Post'/>
+        </form>
+
+        <div class='comments'>
+        EOT;
+    } else {
+        echo "You need to login to post a comment.";
+    }
+
     foreach ($comments as $comment) {
         $comment['diff'] = diff($comment['creation_timestamp']);
         echo <<<EOT
